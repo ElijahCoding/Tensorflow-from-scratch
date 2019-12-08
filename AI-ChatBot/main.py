73a7d3f9779291e013c14722eb4c49faa2233d66
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-08 21:50:50 +0300

    data (words) preprocessing built

--- AI-ChatBot/main.py
+++ AI-ChatBot/main.py
@@ -1,13 +1,33 @@
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 import numpy
-import tflearn
 import tensorflow as tf
 import random
 import json
 
 stemmer = LancasterStemmer()
+nltk.download('punkt')
 
 with open('intents.json') as file:
     data = json.load(file)
-    print(data)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent["patterns"]:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent["tag"])
+
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+print(labels)
