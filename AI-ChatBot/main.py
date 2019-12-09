7c708533cd6d713b87c939d34138ed79585ea37a
ElijahCoding <cuiyeqing960904@gmail.com>
2019-12-09 08:29:29 +0300

    bag of words built

--- AI-ChatBot/main.py
+++ AI-ChatBot/main.py
@@ -30,4 +30,29 @@ words = [stemmer.stem(w.lower()) for w in words]
 words = sorted(list(set(words)))
 
 labels = sorted(labels)
-print(labels)
+
+training = []
+output = []
+
+out_empty = [0 for _ in range(len(labels))]
+
+# Bag of words (one hot encoding)
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+    training = numpy.array(training)
+    output = numpy.array(output)
